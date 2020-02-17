d19303b9d4b936d3b97155f3c0f2b410b23eadde
ihsancemil <ihsancemil@gmail.com>
2020-02-17 22:19:31 +0300

    Aim create and read implemented

--- lib/repository/aim_model.dart
+++ lib/repository/aim_model.dart
@@ -1,4 +1,6 @@
+import 'package:aimio/helpers/database_provider.dart';
 import 'package:aimio/repository/base_repository.dart';
+import 'package:aimio/repository/icon_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class AimModel extends BaseRepository
@@ -9,8 +11,9 @@ class AimModel extends BaseRepository
   final double start;
   final double end;
   final int iconId;
+  final IconModel iconModel;
 
-  AimModel({this.id, this.title, this.definition, this.start, this.end, this.iconId});
+  AimModel({this.id, this.title, this.definition, this.start, this.end, this.iconId, this.iconModel});
 
   @override
   void createTable(Database db) {
@@ -18,5 +21,38 @@ class AimModel extends BaseRepository
       "CREATE TABLE" + 
       "aim (id INTEGER PRIMARY KEY, title TEXT, definition TEXT, start REAL, end REAL, iconId INTEGER)"
     );
-  } 
+  }
+
+  factory AimModel.fromMap(Map<String, dynamic> map) => AimModel(
+    id: map["id"],
+    title: map["title"],
+    definition: map["definition"],
+    start: map["start"],
+    end: map["end"],
+    iconId: map["iconId"],
+    iconModel: IconModel.fromMap(map),
+  );
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["id"] = this.id;
+    map["title"] = this.title;
+    map["definition"] = this.definition;
+    map["start"] = this.start;
+    map["end"] = this.end;
+    map["iconId"] = this.iconId;
+    return map;
+  }
+
+  static Future<List<AimModel>> getAims() async {
+    var db = await DatabaseProvider.db.database;
+    var resources = (await db.query("SELECT * FROM aim JOIN icon ON aim.iconId = icon.id"));
+    return resources.map((map) => AimModel.fromMap(map));
+  }
+
+  static Future<void> newAim(AimModel aim) async {
+    var db = await DatabaseProvider.db.database;
+    db.insert("aim", aim.toMap());
+    db.insert("icon", aim.iconModel.toMap());
+  }
 }
--- lib/repository/icon_model.dart
+++ lib/repository/icon_model.dart
@@ -8,13 +8,31 @@ class IconModel extends BaseRepository {
   final String fontPackage;
   final bool matchTextDirection;
 
-  IconModel({this.id, this.codePoint, this.fontFamily, this.fontPackage, this.matchTextDirection}); 
+  IconModel({this.id, this.codePoint, this.fontFamily, this.fontPackage, this.matchTextDirection});
+
+  factory IconModel.fromMap(Map<String, dynamic> map) => IconModel (
+    id: map["id"],
+    codePoint: map["codePoint"],
+    fontFamily: map["fontFamily"],
+    fontPackage: map["fontPackage"],
+    matchTextDirection: map["matchTextDirection"],
+  );
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map;
+    map["id"] = this.id;
+    map["codePoint"] = this.codePoint;
+    map["fontFamily"] = this.fontFamily;
+    map["fontPackage"] = this.fontPackage;
+    map["matchTextDirection"] = this.matchTextDirection;
+    return map;
+  }
   
   @override
   void createTable(Database db) {
     db.execute(
       "CREATE TABLE" + 
-      "aim (id INTEGER PRIMARY KEY, codePoint INTEGER, fontFamily TEXT, fontPackage TEXT, end REAL, matchTextDirection NUMERIC)"
+      "icon (id INTEGER PRIMARY KEY, codePoint INTEGER, fontFamily TEXT, fontPackage TEXT, end REAL, matchTextDirection NUMERIC)"
     );
   }
 }
